9ab1b874ad2f8123668c224ac47017ff3b4984ce
ptrefall@gmail.com <ptrefall@gmail.com@719f0d19-534f-abe1-c68e-54d7a3c86617>
2012-05-13 18:37:59 +0000

    Added texture 2d array object support.

--- src/File/TextureLoader.cpp
+++ src/File/TextureLoader.cpp
@@ -20,6 +20,45 @@ TextureLoader::TextureLoader(const std::string &base_dir)
 
 Render::Tex2DPtr TextureLoader::load(const std::string &filename, unsigned int wrap_mode)
 {
+	internal_tex_data data = internal_load(filename);
+
+	Render::T2DTexParams params(data.format, data.format, GL_UNSIGNED_BYTE, data.w, data.h, wrap_mode, data.data);
+	auto tex = std::make_shared<Render::Tex2D>(params);
+
+	return tex;
+}
+
+Render::Tex2DArrayPtr TextureLoader::load_array(const std::string &filename, unsigned int width, unsigned int height, unsigned int slice_count, unsigned int wrap_mode)
+{
+	internal_tex_data data = internal_load(filename);
+	unsigned int w = data.w / slice_count;
+	unsigned int h = data.h / slice_count;
+
+	std::vector<unsigned char> data_array;
+
+	for(unsigned int i = 0; i < slice_count; i++)
+	{
+		for(unsigned int x = 0; x < w; x++)
+		{
+			for(unsigned int y = 0; y < h; y++)
+			{
+
+				data_array.push_back(data.data[(slice_count * (x+y*w))*data.bpp + 0]); //Red color
+				data_array.push_back(data.data[(slice_count * (x+y*w))*data.bpp + 1]); //Green color
+				data_array.push_back(data.data[(slice_count * (x+y*w))*data.bpp + 2]); //Blue color
+				if(data.bpp == 4)
+					data_array.push_back(data.data[(slice_count * (x+y*w))*data.bpp + 3]); //Alpha color
+			}
+		}
+	}
+
+	Render::Tex2DArrayParams params(data.format, data.format, GL_UNSIGNED_BYTE, w,h,slice_count,wrap_mode,&data_array[0]);
+	auto tex = std::make_shared<Render::Tex2DArray>(params);
+	return tex;
+}
+
+TextureLoader::internal_tex_data TextureLoader::internal_load(const std::string &filename)
+{
 	ILuint img_id;
     ilGenImages(1, &img_id);
 
@@ -28,7 +67,7 @@ Render::Tex2DPtr TextureLoader::load(const std::string &filename, unsigned int w
     if(!success)
     {
             ILenum err = ilGetError();
-            return nullptr;
+            return internal_tex_data();
     }
 
     if(ilGetInteger(IL_IMAGE_ORIGIN) != IL_ORIGIN_LOWER_LEFT)
@@ -40,17 +79,14 @@ Render::Tex2DPtr TextureLoader::load(const std::string &filename, unsigned int w
     if(!success)
     {
             ILenum err = ilGetError();
-            return nullptr;
+            return internal_tex_data();
     }
 
-    int bpp = ilGetInteger(IL_IMAGE_BPP);
-    int w = ilGetInteger(IL_IMAGE_WIDTH);
-    int h = ilGetInteger(IL_IMAGE_HEIGHT);
-    int format = ilGetInteger(IL_IMAGE_FORMAT);
-    unsigned char *data = ilGetData();
-
-	Render::T2DTexParams params(format, format, GL_UNSIGNED_BYTE, w, h, wrap_mode, data);
-	auto tex = std::make_shared<Render::Tex2D>(params);
-
-	return tex;
+	internal_tex_data data;
+    data.bpp = ilGetInteger(IL_IMAGE_BPP);
+    data.w = ilGetInteger(IL_IMAGE_WIDTH);
+    data.h = ilGetInteger(IL_IMAGE_HEIGHT);
+    data.format = ilGetInteger(IL_IMAGE_FORMAT);
+    data.data = ilGetData();
+	return data;
 }
--- src/File/TextureLoader.h
+++ src/File/TextureLoader.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../Render/Tex2D.h"
+#include "../Render/Tex2DArray.h"
 
 #include <memory>
 #include <string>
@@ -16,8 +17,19 @@ namespace File
 		TextureLoader(const std::string &base_dir);
 
 		Render::Tex2DPtr load(const std::string &filename, unsigned int wrap_mode = GL_CLAMP_TO_EDGE);
+		Render::Tex2DArrayPtr load_array(const std::string &filename, unsigned int width, unsigned int height, unsigned int slice_count, unsigned int wrap_mode = GL_CLAMP_TO_EDGE);
 
 	private:
+		struct internal_tex_data
+		{
+			int bpp;
+			int w;
+			int h;
+			int format;
+			unsigned char *data;
+			internal_tex_data() : bpp(0), w(0), h(0), format(0), data(nullptr) {}
+		};
+		internal_tex_data internal_load(const std::string &filename);
 		std::string base_dir;
 	};
 }
