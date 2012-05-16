331a2f9c809fc441eea166d5856a8243ed34f731
ptrefall@gmail.com <ptrefall@gmail.com@719f0d19-534f-abe1-c68e-54d7a3c86617>
2012-05-16 07:41:34 +0000

    Added new basic main.cpp, CMakeLists.txt, now using a config.h to define default values used in engine, and reads an engine.ini file at startup that defines some initializing values.

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -1,63 +1,69 @@
+
 #include <GL3\gl3.h>
 #include <GL3\gl3w.h>
-#include <GL/freeglut.h>
-#include <glm/glm.hpp>
-
-#include "Render\DeferredRender.h"
-#include "Render\GBuffer.h"
-#include "Raytracer\OptixRender.h"
-#include "File\ShaderLoader.h"
-#include "File\TextureLoader.h"
-#include "File\MaterialLoader.h"
-#include "Scene\SceneManager.h"
-#include "Scene\Cube.h"
-#include "Scene\proto_camera.h"
-
-#include "Render\Shader.h"
-
+#include <GL\freeglut.h>
 #include <string>
+
+#include "config.h"
+#include "Parser\INIParser.h"
 
 void display();
 void reshape(int w, int h);
 void keyboard(unsigned char key, int x, int y);
-void motion(int x, int y);
-int init(int argc, char** argv);
-void loadScene();
-
-//Globals available only to this file
-namespace 
-{
-	Render::GBufferPtr g_buffer;
-	Render::DeferredRenderPtr renderer;
-    Raytracer::OptixRenderRenderPtr raytracer;
-	File::ShaderLoaderPtr shader_loader;
-	File::TextureLoaderPtr tex_loader;
-	File::MaterialLoaderPtr mat_loader;
-	Scene::SceneManagerPtr scene;
-  
-    Scene::FirstPersonCameraPtr camera;
-	glm::ivec2 mouse;
-
-	unsigned int width, height;
-}
+void special(int key, int x, int y);
+void init(int argc, char** argv);
 
 int main(int argc, char** argv)
 {
+	//////////////////////////////////////////////
+	// SET UP SAFE RESOURCE BASE DIRECTORY LOOKUP
+	//////////////////////////////////////////////
+	std::string resource_dir = argv[0];
+	resource_dir = resource_dir.substr(0, resource_dir.find_last_of("\\"));
+	resource_dir = resource_dir.substr(0, resource_dir.find_last_of("\\"));
+	resource_dir += "\\resources\\";
+
+	//////////////////////////////////////////////
+	// PARSE ENGINE SETTINGS
+	//////////////////////////////////////////////
+	ini::Parser parser(resource_dir + "ini\\engine.ini");
+	int width = parser.getInt("Dimensions", "width", ENGINE_DEFAULT_WINDOW_WIDTH);
+	int height = parser.getInt("Dimensions", "height", ENGINE_DEFAULT_WINDOW_HEIGHT);
+	int depth = parser.getInt("Dimensions", "depth", ENGINE_DEFAULT_WINDOW_DEPTH);
+	int refresh_rate = parser.getInt("Dimensions", "refresh_rate", ENGINE_DEFAULT_WINDOW_REFRESH_RATE);
+	int fullscreen = parser.getInt("Modes", "fullscreen", ENGINE_DEFAULT_FULLSCREEN);
+	int game_mode = parser.getInt("Modes", "game", ENGINE_DEFAULT_GAME_MODE);
+
 	//////////////////////////////////////////
 	// GLUT INITIALIZING
 	//////////////////////////////////////////
 	glutInit(&argc, argv);
 	glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-	glutInitContextVersion (4, 2);
+	glutInitContextVersion (ENGINE_OPENGL_VERSION_MAJOR, ENGINE_OPENGL_VERSION_MINOR);
 	glutInitContextFlags (GLUT_FORWARD_COMPATIBLE | GLUT_DEBUG);
 	glutInitContextProfile(GLUT_CORE_PROFILE);
-
-	width = 1280;
-	height = 800;
-
-	glutInitWindowSize (width, height); 
-	glutInitWindowPosition (100, 100);
-	glutCreateWindow (argv[0]);
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+	if(game_mode)
+	{
+		std::stringstream ss;
+		ss << width << "x" << height << ":" << depth << "@" << refresh_rate;
+		glutGameModeString(ss.str().c_str());
+		if(glutGameModeGet(GLUT_GAME_MODE_POSSIBLE))
+			glutEnterGameMode();
+		else
+		{
+			system("pause");
+			return -1;
+		}
+	}
+	else 	
+	{
+		glutInitWindowSize (width, height); 
+		glutInitWindowPosition (100, 100);
+		glutCreateWindow(argv[0]);
+		if(fullscreen)
+			glutFullScreen();
+	}
 
 	//////////////////////////////////////////
 	// GL3W INITIALIZING
@@ -65,14 +71,12 @@ int main(int argc, char** argv)
 	GLenum gl3wInitErr = gl3wInit();
 	if(gl3wInitErr)
 		throw std::runtime_error("Failed to initialize OpenGL!");
-	if(gl3wIsSupported(3,3) == false)
-		throw std::runtime_error("OpenGL 3.3 is not supported!");
-
-	//////////////////////////////////////////
-	// GAME INITIALIZING
-	//////////////////////////////////////////
-  if(init(argc, argv))
-		return -1; 
+	if(gl3wIsSupported(ENGINE_OPENGL_VERSION_MAJOR,ENGINE_OPENGL_VERSION_MINOR) == false)
+	{
+		std::stringstream ss;
+		ss << "Opengl " << ENGINE_OPENGL_VERSION_MAJOR << "." << ENGINE_OPENGL_VERSION_MINOR << " is not supported!";
+		throw std::runtime_error(ss.str());
+	}
 
 	//////////////////////////////////////////
 	// GLUT FUNCTOR INITIALIZING
@@ -81,138 +85,45 @@ int main(int argc, char** argv)
 	glutDisplayFunc(display); 
 	glutReshapeFunc(reshape);
 	glutKeyboardFunc(keyboard);
-	glutMotionFunc(motion);
+	glutSpecialFunc(special);
+
+	//////////////////////////////////////////
+	// CUSTOM INITIALIZATION
+	//////////////////////////////////////////
+	init(argc,argv);
 
 	//////////////////////////////////////////
 	// HEARTBEAT
 	//////////////////////////////////////////
 	glutMainLoop();
-
-	return 0;
 }
 
 void display()
-{  
-	camera->update(false, false, false, false, (float)mouse.x/(float)width, (float)mouse.y/(float)height, true, 1.0f);
-  glClearColor(0.f,0.f,0.f,1.f);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+{
+	glClearColor(0.f,0.f,0.f,1.f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
-
-    //Rasterize
-  glEnable(GL_DEPTH_TEST);
-	g_buffer->begin();
-	scene->render(g_buffer->getShader());
-	g_buffer->end();
-  glDisable(GL_DEPTH_TEST);
-	renderer->render();
-
-    //Raytrace
-    //raytracer->render();
-
+	//CUSTOM RENDER CODE GOES HERE
 
 	glutSwapBuffers();
 }
 
 void reshape(int w, int h)
 {
-	g_buffer->reshape(w,h);
-	renderer->reshape(w,h);
 }
 
 void keyboard(unsigned char key, int x, int y)
 {
-	if ( key=='R' || key=='r' ) {
-		system("cls");
-		renderer->reloadShaders();
-	}
-	/*auto cam = Scene::Camera::getSingleton();
-	if(key=='a'){
-		cam->getOrientation() = glm::rotate(cam->getOrientation(), 0.1f, glm::vec3(0.0, 1.0, 0.0));
-	}else if(key == 'd'){
-		cam->getOrientation() = glm::rotate(cam->getOrientation(), -0.1f, glm::vec3(0.0, 1.0, 0.0));
-	}*/
+	//ESCAPE KEY
+	if(key == 27)
+		glutLeaveMainLoop();
 }
 
-void motion(int x, int y)
+void special(int key, int x, int y)
 {
-	mouse.x = x;
-	mouse.y = y;
 }
 
-int init(int argc, char** argv)
+void init(int argc, char** argv)
 {
-	//////////////////////////////////////////////
-	// SET UP SAFE RESOURCE BASE DIRECTORY LOOKUP
-	//////////////////////////////////////////////
-	std::string base_dir(argv[0]);
-	base_dir = base_dir.substr(0, base_dir.find_last_of("\\"));
-	base_dir = base_dir.substr(0, base_dir.find_last_of("\\"));
-	base_dir += "\\resources\\";
-
-	//////////////////////////////////////////
-	// FILE SYSTEM INITIALIZING
-	//////////////////////////////////////////
-	shader_loader = std::make_shared<File::ShaderLoader>(base_dir+"shaders\\");
-	tex_loader = std::make_shared<File::TextureLoader>(base_dir+"textures\\");
-	mat_loader = std::make_shared<File::MaterialLoader>(base_dir+"materials\\");
-
-
-	//////////////////////////////////////////
-	// DEFERRED RENDERER INITIALIZING
-	//////////////////////////////////////////
-	g_buffer = std::make_shared<Render::GBuffer>(shader_loader, width, height);
-	renderer = std::make_shared<Render::DeferredRender>(g_buffer, shader_loader, width, height);
-
-    //////////////////////////////////////////
-	// DEFERRED RENDERER INITIALIZING
-	//////////////////////////////////////////
-    //raytracer = std::make_shared<Raytracer::OptixRender>(g_buffer, width, height, base_dir + "optix\\");
-
-	//////////////////////////////////////////
-	// SCENE INITIALIZING
-	//////////////////////////////////////////
-	scene = std::make_shared<Scene::SceneManager>();
-	loadScene();
-	return 0;
-}
-
-void loadScene()
-{
-    camera = Scene::FirstPersonCamera::getSingleton();
-	camera->updateProjection(width, height, 90, 1.0f, 1000.0f);
-    //camera->init(width, height, 60.0f, 1.0f, 1000.0f);
-	//camera->setTarget(glm::vec3(10,-8,20));
-
-	//auto cube_tex = tex_loader->load("cube.jpg", GL_REPEAT);
-	//auto tex_sampler = std::make_shared<Render::Uniform>(g_buffer->getShader()->getFS(), "diffuse_tex");
-
-	//auto array_tex = tex_loader->load_array("array.png", 16, 16, 2, 2, GL_REPEAT);
-	auto array_tex = tex_loader->load("terminal.png", GL_REPEAT);
-	auto array_sampler = std::make_shared<Render::Sampler>(GL_REPEAT);
-
-	auto basic_cube_mat = renderer->addMaterial(mat_loader->load("basic_cube.mat"));
-	auto red_cube_mat = renderer->addMaterial(mat_loader->load("red_cube.mat"));
-
-	Scene::CubePtr cube;
-	cube = std::make_shared<Scene::Cube>(1.0f);
-	{
-		cube->setMVP(	g_buffer->getMVP());
-		cube->setMV(	g_buffer->getMV());
-		cube->setN_WRI(	g_buffer->getN_WRI());
-		cube->setTexture(array_tex, array_sampler);
-		cube->setMaterial(basic_cube_mat);
-		scene->add(cube);
-		cube->setPosition( glm::vec3(10,-8,20) );
-	}
-
-	Scene::CubePtr cube2 = std::make_shared<Scene::Cube>(.5f);
-	{
-		cube2->setMVP(	g_buffer->getMVP());
-		cube2->setMV(	g_buffer->getMV());
-		cube2->setN_WRI(	g_buffer->getN_WRI());
-		cube2->setTexture(array_tex, array_sampler);
-		cube2->setMaterial(red_cube_mat);
-		scene->add(cube2);
-		cube2->setPosition( glm::vec3(10,-4,20) );
-	}
+	//CUSTOM INITIALIZATION CODE GOES HERE
 }
