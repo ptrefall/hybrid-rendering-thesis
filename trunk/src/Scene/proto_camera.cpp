a060408c28ecc6e9c7f9ba9129cf6bf006270fe8
PTrefall@gmail.com <PTrefall@gmail.com@719f0d19-534f-abe1-c68e-54d7a3c86617>
2012-05-22 17:11:04 +0000

    still wondering why cameras dont seem to be synced. Doing raytracer::render before scene and deferred render, as it is composited in deferredRenderer.

--- trunk/src/Scene/proto_camera.cpp
+++ trunk/src/Scene/proto_camera.cpp
@@ -22,48 +22,54 @@ void FirstPersonCamera::Shutdown()
 	}
 }
 
-const glm::mat4 &FirstPersonCamera::updateProjection(unsigned int w, unsigned int h, float fov, float near, float far)
+
+FirstPersonCamera::FirstPersonCamera() 
+/*    : projection ( glm::mat4(1.f) )
+    ,view( glm::mat4(1.f) )
+    ,pos( glm::vec3(0.f) )
+    ,cameraStrafe( glm::vec3(1.f, 0.f, 0.f) )
+    ,cameraUp ( glm::vec3(0.f, 1.f, 0.f) )
+    ,cameraForward ( glm::vec3(0.f, 0.f, 1.f) )
+    ,vFov (45.f)
+    ,far_dist (1.f)
+    ,near_dist (1000.f)
+    ,movementUnitsPerSecond (50.f)
+    ,mouseDegreesPerSecond(10.f)
+    ,hang (0.f)
+    ,vang (0.f)
+    ,oldmousx (0.f)
+    ,oldmousy (0.f)*/ 
 {
-	projection = glm::perspective<float>(fov, w/(float)h, near, far);
-	return projection;
+    projection = glm::mat4(1.f);
+    view = glm::mat4(1.f);
+	pos = glm::vec3(0.f);
+	cameraStrafe = glm::vec3(1.f, 0.f, 0.f);
+    cameraUp= glm::vec3(0.f, 1.f, 0.f);
+    cameraForward = glm::vec3(0.f, 0.f, 1.f);
+	vFov = 45.f;
+	far_dist = 1.f;
+    near_dist = 1000.f;
+    movementUnitsPerSecond = 50.f;
+    mouseDegreesPerSecond = 10.f;
+    hang = 0.f;
+    vang = 0.f;
+	oldmousx = 0.f;
+	oldmousy = 0.f;
+
+    pos = glm::vec3(0,0,-45);
+    hang = glm::radians( 35.f );
 }
 
-FirstPersonCamera::FirstPersonCamera()
+const glm::mat4 &FirstPersonCamera::updateProjection(unsigned int w, unsigned int h, float vFov, float near, float far)
 {
-	mCam = glm::mat4(1.0f);
-	pos = glm::vec3(0.f);
-
-	cameraStrafe =  glm::vec3(1.f, 0.f, 0.f);
-	cameraUp =      glm::vec3(0.f, 1.f, 0.f);
-	cameraForward = glm::vec3(0.f, 0.f, 1.f);
-
-	fov = 45.f;
-	near_dist = 1.0f;
-	far_dist = 1000.f;
-
-	hang = 0.0f;
-	vang = 0.0f;
-
-	oldmousx = 0.0f;
-	oldmousy = 0.0f;
+	projection = glm::perspective<float>(vFov, w/(float)h, near, far);
+	return projection;
 }
 
 void FirstPersonCamera::lookAt( const glm::vec3& pos, const glm::vec3& target, const glm::vec3& up )
 {
 	setPos(pos);
-	mCam = glm::lookAt( pos, target, up );
-}
-
-glm::vec3 SphericalToCartesian(float polar, float elevation){
-	float radius = 1.f;
-	float a = radius * cos(elevation);
-	return glm::vec3( a * cos(polar), radius * sin(elevation), a * sin(polar) );
-}
-
-void FirstPersonCamera::set( glm::vec3 const& pos, float horiz_degrees, float verti_degrees ) {
-	setPos(pos);
-	glm::vec3 target = SphericalToCartesian(hang,vang); 
-	mCam = glm::lookAt( pos, target, glm::vec3(0.f, 1.f, 0.f) );
+	view = glm::lookAt( pos, target, up );
 }
 
 //void FirstPersonCamera::quat_code()
@@ -76,44 +82,70 @@ void FirstPersonCamera::set( glm::vec3 const& pos, float horiz_degrees, float ve
 void FirstPersonCamera::update(bool left_key, bool right_key, bool back_key, bool forwards_key,
 	float mouse_x, float mouse_y, bool mouse_is_down, float delta )
 {
-	float mouse_speed_x = mouse_x - oldmousx;
-	float mouse_speed_y = mouse_y - oldmousy;
+	float mouse_speed_x = delta * mouseDegreesPerSecond * (mouse_x - oldmousx);
+	float mouse_speed_y = delta * mouseDegreesPerSecond * (mouse_y - oldmousy);
 	oldmousx = mouse_x;
 	oldmousy = mouse_y;
 	if ( mouse_is_down )
 	{	
-		hang += mouse_speed_x * 0.1f;
-		vang += mouse_speed_y * 0.1f;
+        hang -= glm::radians(mouse_speed_x);
+		vang -= glm::radians(mouse_speed_y);
 	}
-	float speed = delta * 50.0f; // meters per second
+	float speed = delta * movementUnitsPerSecond; // meters per second
 
-	glm::mat4 rmx = glm::rotate( glm::mat4(1.0f), vang, cameraStrafe );
-	glm::mat4 rmy = glm::rotate( glm::mat4(1.0f), hang, cameraUp );
-	glm::mat4 rotMat = rmx * rmy;
+    // Direction : Spherical coordinates to Cartesian coordinates conversion
+    glm::vec3 lookDir(
+        cos(vang) * sin(hang),
+        sin(vang),
+        cos(vang) * cos(hang)
+    );
 
-	//glm::vec4 moveDir( speed * (left_key-right_key), 0.0f, speed * (back_key - forwards_key), 0.0f );
-	glm::vec4 moveDir( speed * (left_key-right_key), 0.0f, speed * (forwards_key-back_key), 0.0f );
-	glm::vec4 lookDir = moveDir * rotMat;
-	glm::vec3 newPos = getPos() + glm::vec3( lookDir );
+    // Right vector
+    glm::vec3 right = glm::vec3(
+        sin(hang - 3.14f/2.0f),
+        0,
+        cos(hang - 3.14f/2.0f)
+    );
 
-	setPos(newPos);
-	mCam = glm::translate(rotMat, newPos);
-}
+    glm::vec3 up = glm::cross( right, lookDir );
 
-glm::mat4 FirstPersonCamera::getViewMatrix()
-{
-	return mCam;
-}
+    float xMove = float(right_key-left_key);
+    float zMove = float(forwards_key-back_key);
+    glm::vec3 moveDir = right*xMove + lookDir*zMove;
+    if ( glm::length2(moveDir) ) 
+        pos += speed * glm::normalize(moveDir);
 
-void FirstPersonCamera::setPos(const glm::vec3& pos ) {
-	/*mCam[3].x = pos.x;
-	mCam[3].y = pos.y;
-	mCam[3].z = pos.z;*/
-	this->pos = pos;
-}
+    cameraStrafe = right;
+    cameraUp = up;
+    cameraForward = lookDir;
 
-glm::vec3 FirstPersonCamera::getPos(){
-	return pos;
-	//return glm::vec3( mCam[3].x, mCam[3].y, mCam[3].z );
-}					  
-					  
+    view = glm::lookAt( pos, pos+lookDir, up );
+
+    if ( 0 ) {
+        glm::vec3 iStr = glm::vec3(view[0].x, view[1].x, view[2].x);
+        glm::vec3 iHei = glm::vec3(view[0].y, view[1].y, view[2].y);
+        glm::vec3 iFor = glm::vec3(view[0].z, view[1].z, view[2].z);
+        glm::vec3 iPos = glm::vec3(view[3].x, view[3].y, view[3].z);
+        printf("\n");
+        printf("%.1f %.1f %.1f\n", cameraStrafe.x, cameraStrafe.y, cameraStrafe.z);
+        printf("%.1f %.1f %.1f\n", cameraUp.x, cameraUp.y, cameraUp.z);
+        printf("%.1f %.1f %.1f\n", cameraForward.x, cameraForward.y, cameraForward.z);
+        printf("%.1f %.1f %.1f\n", pos.x, pos.y, pos.z);
+        printf("\n");
+        printf("%.1f %.1f %.1f\n", iStr.x, iStr.y, iStr.z);
+        printf("%.1f %.1f %.1f\n", iHei.x, iHei.y, iHei.z);
+        printf("%.1f %.1f %.1f\n", iFor.x, iFor.y, iFor.z);
+        printf("%.1f %.1f %.1f\n", iPos.x, iPos.y, iPos.z);
+    }
+
+	//glm::mat4 rmx = glm::rotate( glm::mat4(1.0f), vang, cameraStrafe );
+	//glm::mat4 rmy = glm::rotate( glm::mat4(1.0f), hang, cameraUp );
+	//glm::mat4 rotMat = rmx * rmy;
+
+	//glm::vec4 moveDir( speed * (left_key-right_key), 0.0f, speed * (forwards_key-back_key), 0.0f );
+	//glm::vec4 lookDir = moveDir * rotMat;
+	//glm::vec3 newPos = getPos() + glm::vec3( lookDir );
+
+	//setPos(newPos);
+	//view = glm::translate(rotMat, newPos);
+}				  
--- trunk/src/Scene/proto_camera.h
+++ trunk/src/Scene/proto_camera.h
@@ -18,33 +18,21 @@ public:
 
 	void lookAt( const glm::vec3& pos, const glm::vec3& target, const glm::vec3& up );
 
-	void set( glm::vec3 const& new_pos, float horiz_degrees, float verti_degrees ) ;
-
 	void update(bool left_key, bool right_key, bool back_key, bool forwards_key,
 		float mouse_x, float mouse_y, bool mouse_is_down, float delta );
 
-	float getHorizontalAngle() {
-		return hang;
-	}
+	float getHorizontalAngle() { return hang; }
 
-	float getVerticalAngle() {
-		return vang;
-	}
+	float getVerticalAngle() { return vang; }
 
-	glm::vec3 getStrafeDirection() 
-	{
-		return glm::vec3( mCam[0].x, mCam[1].x, mCam[2].x );
-	}
+	glm::vec3 getStrafeDirection() { return glm::vec3(view[0].x, view[1].x, view[2].x); }
 
-	glm::vec3 getUpDirection() 
-	{
-		return glm::vec3( mCam[0].y, mCam[1].y, mCam[2].y );
-	}
+	glm::vec3 getUpDirection() { return glm::vec3(view[0].y, view[1].y, view[2].y); }
 
-	glm::vec3 getLookDirection() 
-	{
-		return -1.f * glm::vec3( mCam[0].z, mCam[1].z, mCam[2].z );
-	}
+	glm::vec3 getLookDirection()  { 
+        // compensate for glm::lookAt reversing zDir as of GL convention
+        return -glm::vec3(view[0].z, view[1].z, view[2].z); 
+    }
 
 	void setCameraBasis( const glm::vec3& cameraStrafe, const glm::vec3& cameraUp, const glm::vec3& cameraForward )
 	{ 
@@ -53,34 +41,41 @@ public:
 		this->cameraForward = cameraForward;
 	}
 
-	float getFov(){return fov;}
-	void setFov( float fov ){ this->fov = fov;}
+	float getFov(){return vFov;}
+	void setFov( float vFov ){ this->vFov = vFov;}
 
+    void setNearDist(float near_dist) { this->near_dist = near_dist; }
 	float getNearDist() { return near_dist; }
+
 	float getFarDist() { return far_dist; }
-	void setNearDist(float near_dist) { this->near_dist = near_dist; }
 	void setFarDist(float far_dist) { this->far_dist = far_dist; }
 
-	glm::mat4 getViewMatrix();
-	void setViewMatrix( const glm::mat4& mat ) { mCam = mat; }
-	void setPos(const glm::vec3& pos );
-	glm::vec3 getPos();
+    void setPos(const glm::vec3& pos ) { this->pos = pos; };
+    const glm::vec3 &getPos() const { return pos; }
 
-	const glm::mat4 &updateProjection(unsigned int w, unsigned int h, float fov, float near, float far);
+	const glm::mat4 &updateProjection(unsigned int w, unsigned int h, float vFov, float near, float far);
 	const glm::mat4 &getProjection() const { return projection; }
+    const glm::mat4 &getViewMatrix() const { return view; }
 
 private:
 	static FirstPersonCameraPtr singleton;
 
 	glm::mat4 projection;
-
-	float hang, vang;
-	glm::mat4 mCam;
+    glm::mat4 view;
+	
 	glm::vec3 pos;
-	glm::vec3 cameraStrafe, cameraUp, cameraForward;
-	float fov;
-	float far_dist, near_dist;
+	glm::vec3 cameraStrafe;
+    glm::vec3 cameraUp;
+    glm::vec3 cameraForward;
 
+	float vFov;
+	float far_dist;
+    float near_dist;
+    float movementUnitsPerSecond;
+    float mouseDegreesPerSecond;
+
+    float hang;
+    float vang;
 	float oldmousx;
 	float oldmousy;
 };
