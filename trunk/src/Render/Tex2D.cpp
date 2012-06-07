d86946958a3555fca67f8906526b1d050270d45d
torbjoernhaugen@gmail.com <torbjoernhaugen@gmail.com@719f0d19-534f-abe1-c68e-54d7a3c86617>
2012-06-07 10:49:52 +0000

    yay, CUDA shared GLBO can now be read by finalColor.fs after generating mipmaps for Tex at init (need glGenTextures glTexture2D glGenerateMipmap), but.. This is strange behaviour, should not need mipmaps...?

--- trunk/src/Render/Tex2D.cpp
+++ trunk/src/Render/Tex2D.cpp
@@ -30,24 +30,7 @@ void Tex2D::init(const T2DTexParams &tex_params)
 	if(handle != 0)
 		return;
 
-	internal_format = tex_params.internal_format;
-	format = tex_params.format;
-	type = tex_params.type;
-	w = tex_params.w;
-	h = tex_params.h;
-	wrap_mode = tex_params.wrap_mode;
-	data = tex_params.data;
-
-	glGenTextures(1, &handle);
-	bind();
-	glTexImage2D(GL_TEXTURE_2D, 0, internal_format, w, h, 0, format, type, data);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrap_mode);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrap_mode);
-	glGenerateMipmap(GL_TEXTURE_2D);
+	update(tex_params);
 }
 
 void Tex2D::update(const T2DTexParams &tex_params)
@@ -65,12 +48,18 @@ void Tex2D::update(const T2DTexParams &tex_params)
 		glGenTextures(1, &handle);
 		bind();
 		glTexImage2D(GL_TEXTURE_2D, 0, internal_format, w, h, 0, format, type, nullptr);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrap_mode);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrap_mode);
+		glGenerateMipmap(GL_TEXTURE_2D);
 	}
 
     if(data)
     {
 	    bind();
-	    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, w, h, GL_BGRA, type, data);
+	    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, w, h, GL_BGRA /* assume cpu data is allways BRGRA? */, type, data);
 	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrap_mode);
