a27db83d9bf9013b8998f50721605bc9387ed181
torbjoernhaugen@gmail.com <torbjoernhaugen@gmail.com@719f0d19-534f-abe1-c68e-54d7a3c86617>
2012-06-07 15:10:29 +0000

    sample2 converted to optixpp api, and now, realtime! spinning camera.

--- trunk/glfw_optix/src/main.cpp
+++ trunk/glfw_optix/src/main.cpp
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+//#include <stdlib.h>
 #include <stdio.h>
 #include <math.h> 
 
@@ -16,44 +16,241 @@
 #include <Render/Shader.h>
 #include <Render/PBO.h>
 #include <Scene/Quad.h>
+#include <glm/glm.hpp>
+
+#include "commonStructs.h"
 
 
 // screen, buffer and tex all same size
-const static int TEX_WIDTH = 1920;
-const static int TEX_HEIGHT =1080;
+const static int TEX_WIDTH = 800;
+const static int TEX_HEIGHT =600;
+
+struct Camera
+{
+	glm::vec3 eyePos;
+	glm::vec3 u;
+	glm::vec3 v;
+	glm::vec3 w;
+} cam;
+
+struct CamVars
+{
+	optix::Variable eyePos;
+	optix::Variable u;
+	optix::Variable v;
+	optix::Variable w;
+} optixCamVars;
+
+void ang2mat(float hang,float vang,float mat[9])
+{
+   mat[0] = cos(hang); mat[1] = 0; mat[2]=-sin(hang);
+   mat[6] = cos(vang)*sin(hang); mat[7] = sin(vang); mat[8]=cos(vang)*cos(hang);
+   mat[3] = mat[7]*mat[2] - mat[8]*mat[1];
+   mat[4] = mat[8]*mat[0] - mat[6]*mat[2];
+   mat[5] = mat[6]*mat[1] - mat[7]*mat[0];
+}
 
 class OptixScene
 {
 public:
-	OptixScene(std::string optix_dir, int width, int height)
+	OptixScene(std::string optix_dir, int width, int height) :
+	  optix_dir(optix_dir), width(width), height(height)
 	{
-		/* Create our objects and set state */
-		context = optix::Context::create();
-		context->setRayTypeCount(1);
-		context->setEntryPointCount(1);
+		init();
+		createInstances( createGeometry(), createMaterial() );
+	}
 
-		std::string path_to_ptx = optix_dir + "\\glfw_optix.cu.ptx";
-		optix::Program ray_gen_program = context->createProgramFromPTXFile( path_to_ptx, "sampleTex" );
+	void init()
+	{
+		cam.eyePos = glm::vec3(0.0f, 0.0f, 5.0f);
+		cam.u = glm::vec3(1.0f, 0.0f, 0.0f ); // screen wid
+		cam.v = glm::vec3(0.0f, 1.0f, 0.0f ); // up
+		cam.w = glm::vec3(0.0f, 0.0f, -1.0f); // Z
+
+		/* Context */
+		context = optix::Context::create();
+		context->setRayTypeCount(2); /* shadow and radiance */
+		context->setEntryPointCount(1);
+		optix::Variable out_buffer = context->declareVariable("output_buffer");
+		optix::Variable light_buffer = context->declareVariable("lights");
+		context->declareVariable("max_depth")->setInt(10);
+		context->declareVariable("radiance_ray_type")->setUint(0u);
+		context->declareVariable("shadow_ray_type")->setUint(1u);
+		context->declareVariable("scene_epsilon")->setFloat(1.e-4f);
+
+		/* Lights buffer */
+		BasicLight light;
+		light.color.x = 0.9f;
+		light.color.y = 0.9f;
+		light.color.z = 0.9f;
+		light.pos.x   = 0.0f;
+		light.pos.y   = 20.0f;
+		light.pos.z   = 20.0f;
+		light.casts_shadow = 1;
+		light.padding      = 0u;
+
+		optix::Buffer light_buffer_obj = context->createBuffer(RT_BUFFER_INPUT, RT_FORMAT_USER, sizeof(BasicLight) );
+		light_buffer_obj->setElementSize( sizeof(BasicLight) );
+		light_buffer_obj->setSize(1);
+		void* light_buffer_data = light_buffer_obj->map();
+		((BasicLight*)light_buffer_data)[0] = light;
+		light_buffer_obj->unmap();
+		light_buffer->set(light_buffer_obj);
+
+		/* Ray gen program */
+		std::string path_to_ptx = optix_dir + "\\pinhole_camera.cu.ptx";
+		optix::Program ray_gen_program = context->createProgramFromPTXFile( path_to_ptx, "pinhole_camera" );
+		optixCamVars.eyePos = ray_gen_program->declareVariable("eye"); 
+		optixCamVars.u = ray_gen_program->declareVariable("U"); 
+		optixCamVars.v = ray_gen_program->declareVariable("V");
+		optixCamVars.w = ray_gen_program->declareVariable("W");
+		optixCamVars.eyePos->set3fv(&cam.eyePos.x);
+		optixCamVars.u->set3fv(&cam.u.x);
+		optixCamVars.v->set3fv(&cam.v.x);
+		optixCamVars.w->set3fv(&cam.w.x);
 
 		unsigned int screenDims[] = {width,height};
 		ray_gen_program->declareVariable("rtLaunchDim")->set2uiv(screenDims);
-
 		fTime = ray_gen_program->declareVariable("fTime");
-		float initTime = (float)glfwGetTime();
-		fTime->set1fv( &initTime );
-
+		fTime->setFloat( (float)glfwGetTime() );
 		context->setRayGenerationProgram(0, ray_gen_program);
 
-		// Create shared GL/CUDA PBO
+		/* Miss program */
+		path_to_ptx = optix_dir + "\\constantbg.cu.ptx";
+		optix::Program miss_program = context->createProgramFromPTXFile( path_to_ptx, "miss" );
+		glm::vec3 missColor(.3f, 0.1f, 0.2f);
+		miss_program->declareVariable("bg_color")->set3fv(&missColor.x);
+		context->setMissProgram(0, miss_program );
+
+		/* Create shared GL/CUDA PBO */
 		int element_size = 4 * sizeof(char);
 		pbo = new Render::PBO(element_size * width * height, GL_STREAM_DRAW, true);
+		//out_buffer_obj = context->createBuffer(RT_BUFFER_OUTPUT, RT_FORMAT_UNSIGNED_BYTE4, width, height);
+		out_buffer_obj = context->createBufferFromGLBO(RT_BUFFER_OUTPUT, pbo->getHandle() );
+		out_buffer_obj->setFormat(RT_FORMAT_UNSIGNED_BYTE4);
+		out_buffer_obj->setSize(width,height);
+		
+		out_buffer->set(out_buffer_obj);
+	}
 
-		buffer = context->createBufferFromGLBO(RT_BUFFER_INPUT_OUTPUT, pbo->getHandle() );
-		buffer->setFormat(RT_FORMAT_UNSIGNED_BYTE4);
-		buffer->setSize(width,height);
+	optix::Geometry createGeometry()
+	{
+		float     box_min[3];
+		float     box_max[3];
+		box_min[0] = box_min[1] = box_min[2] = -0.5f;
+		box_max[0] = box_max[1] = box_max[2] =  0.5f;
+		optix::Geometry box_geo = context->createGeometry();
+		box_geo->setPrimitiveCount(1u);
 
-		optix::Variable out_buffer = context->declareVariable("out_buffer");
-		out_buffer->set(buffer);
+		std::string path_to_ptx = optix_dir + "\\box.cu.ptx";
+		optix::Program box_bounding_box_program = context->createProgramFromPTXFile( path_to_ptx, "box_bounds" );
+		optix::Program box_intersection_program = context->createProgramFromPTXFile( path_to_ptx, "box_intersect" );
+		box_geo->setBoundingBoxProgram(box_bounding_box_program);
+		box_geo->setIntersectionProgram(box_intersection_program);
+		box_geo->declareVariable("boxmin")->set3fv(box_min);
+		box_geo->declareVariable("boxmax")->set3fv(box_max);
+
+		return box_geo;
+	}
+	
+	optix::Material createMaterial()
+	{
+		/* Create our hit programs to be shared among all materials */
+		std::string path_to_ptx = optix_dir + "\\phong.cu.ptx";
+		optix::Program closest_hit_program = context->createProgramFromPTXFile( path_to_ptx, "closest_hit_radiance" );
+		optix::Program any_hit_program = context->createProgramFromPTXFile( path_to_ptx, "any_hit_shadow" );
+
+		optix::Material mat = context->createMaterial();
+	   /* Note that we are leaving anyHitProgram[0] and closestHitProgram[1] as NULL.
+		 * This is because our radiance rays only need closest_hit and shadow rays only
+		 * need any_hit */
+		mat->setClosestHitProgram(0, closest_hit_program);
+		mat->setAnyHitProgram(1, any_hit_program);
+		return mat;
+	}
+
+	void createInstances(optix::Geometry box, optix::Material material)
+	{
+		static const int NUM_BOXES = 6;
+		optix::Transform transforms[NUM_BOXES];
+
+		float m[16];
+		m[ 0] = 1.0f;  m[ 1] = 0.0f;  m[ 2] = 0.0f;  m[ 3] = 0.0f;
+		m[ 4] = 0.0f;  m[ 5] = 1.0f;  m[ 6] = 0.0f;  m[ 7] = 0.0f;
+		m[ 8] = 0.0f;  m[ 9] = 0.0f;  m[10] = 1.0f;  m[11] = 0.0f;
+		m[12] = 0.0f;  m[13] = 0.0f;  m[14] = 0.0f;  m[15] = 1.0f;
+
+
+		for ( int i = 0; i < NUM_BOXES; ++i )
+		{
+			/* Create this geometry instance */
+			optix::GeometryInstance instance = context->createGeometryInstance();
+			instance->setGeometry(box);
+			instance->setMaterialCount(1);
+			instance->setMaterial(0, material);
+			
+			/* Set variables to be consumed by material for this geometry instance */
+			struct material_data_def
+			{
+				glm::vec3 ambient;
+				glm::vec3 kd, ks, ka;
+				glm::vec3 reflectivity;
+				float expv;
+			} material_data;
+			float kd_slider = (float)i / (float)(NUM_BOXES-1);
+			material_data.kd = glm::vec3(kd_slider, 0.0f, 1.0f-kd_slider);
+			material_data.ks = glm::vec3(0.5f, 0.5f, 0.5f);
+			material_data.ka = glm::vec3(0.8f, 0.8f, 0.8f);
+			material_data.reflectivity = glm::vec3(0.8f, 0.8f, 0.8f);
+			material_data.expv = 10.0f;
+			material_data.ambient = glm::vec3(0.2f, 0.2f, 0.2f);
+
+			optix::Variable kd = instance->declareVariable("Kd");
+			optix::Variable ks = instance->declareVariable("Ks");
+			optix::Variable ka = instance->declareVariable("Ka");
+			optix::Variable expv = instance->declareVariable("phong_exp");
+			optix::Variable reflectivity = instance->declareVariable("reflectivity");
+			optix::Variable ambient = instance->declareVariable("ambient_light_color");
+			kd->set3fv( &material_data.kd.x );
+			ks->set3fv( &material_data.ks.x );
+			ka->set3fv( &material_data.ka.x );
+			reflectivity->set3fv( &material_data.reflectivity.x );
+			expv->setFloat( material_data.expv );
+			ambient->set3fv( &material_data.ambient.x );
+
+			/* create group to hold instance transform */
+			optix::GeometryGroup geometrygroup = context->createGeometryGroup();
+			geometrygroup->setChildCount(1);
+			geometrygroup->setChild(0,instance);
+
+			/* create acceleration object for group and specify some build hints*/
+			optix::Acceleration acceleration = context->createAcceleration("NoAccel", "NoAccel");
+			geometrygroup->setAcceleration(acceleration);
+
+			/* mark acceleration as dirty */
+			acceleration->markDirty();
+
+			transforms[i] = context->createTransform();
+			transforms[i]->setChild( geometrygroup );
+			m[3] = i*1.5f - (NUM_BOXES-1)*0.75f;
+			transforms[i]->setMatrix( 0, m, 0 );
+		}
+
+		/* Place these geometrygroups as children of the top level object */
+		optix::Group top_level_group = context->createGroup();
+		top_level_group->setChildCount(NUM_BOXES);
+		for(int i=0; i<NUM_BOXES; ++i){
+			top_level_group->setChild(i, transforms[i] );
+		}
+		optix::Variable top_object = context->declareVariable("top_object");
+		top_object->set( top_level_group );
+		optix::Variable top_shadower = context->declareVariable("top_shadower");
+		top_shadower->set(top_level_group);
+		optix::Acceleration top_level_acceleration = context->createAcceleration("NoAccel", "NoAccel");
+		top_level_group->setAcceleration(top_level_acceleration);
+
+		/* mark acceleration as dirty */
+		top_level_acceleration->markDirty();
 	}
 
 	optix::Variable getFTime()
@@ -63,34 +260,46 @@ public:
 
 	optix::Buffer getOutBuffer()
 	{
-		return buffer;
+		return out_buffer_obj;
 	}
 
-	optix::Program getRayGen()
+	void animate()
 	{
-		return context->getRayGenerationProgram(0);
+		float t = fTime->getFloat();
+		float r = 3.f;
+		cam.eyePos.x = r*sin(t);
+		cam.eyePos.y = 0;
+		cam.eyePos.z = r*cos(t);
+
+		float mtx[9];
+		ang2mat(t,0.f, mtx);
+
+		cam.u = glm::vec3( mtx[0],mtx[1],mtx[2] );
+		cam.v = glm::vec3( mtx[3],mtx[4],mtx[5] );
+		cam.w = -glm::vec3( mtx[6],mtx[7],mtx[8] );
+		
+		optixCamVars.eyePos->set3fv(&cam.eyePos.x);
+		optixCamVars.u->set3fv(&cam.u.x);
+		optixCamVars.v->set3fv(&cam.v.x);
+		optixCamVars.w->set3fv(&cam.w.x);
+
 	}
 
 	~OptixScene()
 	{
-		/* Clean up */
-		try {
-			buffer->unregisterGLBuffer();
-			buffer->destroy();
-			getRayGen()->destroy();
-			context->destroy();
-		} catch( std::exception &e) {
-			//std::cout << e.what() << std::endl;
-			printf("%s\n", e.what() );
-			system("pause");
-		}
+		out_buffer_obj->unregisterGLBuffer();
+		context->destroy();
 	}
 public:
 	Render::PBO *pbo;
 private:
 	optix::Context context;
-	optix::Buffer buffer;
+	optix::Buffer out_buffer_obj;
 	optix::Variable fTime;
+
+	std::string optix_dir;
+	int width;
+	int height;
 };
 
 class GLContext
@@ -226,6 +435,8 @@ public:
 		optix::Buffer imageBuffer = myScene->getOutBuffer();
 		optix::Variable fTime = myScene->getFTime();
 		optix::Context context = imageBuffer->getContext();
+
+
 		context->validate();
 		context->compile();
 
@@ -238,6 +449,7 @@ public:
 			glfwSetWindowTitle(wnd, titleBuf );*/
 			fTime->set1fv( &time );
 
+			myScene->animate();
 			context->launch(0 /*entry point*/, width, height );
 
 			glActiveTexture(GL_TEXTURE0 + 0);
@@ -285,8 +497,17 @@ int main(int argc, char* argv[])
 	unsigned int height = TEX_HEIGHT;
 
 	GLContext myGLContext(width,height);
-	OptixScene *myScene = new OptixScene(optix_dir, width, height);
-	myGLContext.display( myScene );
+	
+	try {
+		OptixScene *myScene = new OptixScene(optix_dir, width, height);
+		myGLContext.display( myScene );
+	} catch( std::exception &e) {
+		//std::cout << e.what() << std::endl;
+		printf("%s\n", e.what() );
+		system("pause");
+	}
+
+	glfwTerminate();
 
 	return 0;
 }
