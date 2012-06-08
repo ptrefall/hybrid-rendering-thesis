f3e5d9e54aa72ca767946efe4b5c16d82750b306
torbjoernhaugen@gmail.com <torbjoernhaugen@gmail.com@719f0d19-534f-abe1-c68e-54d7a3c86617>
2012-06-08 07:09:27 +0000

    vastly improved proto_camera, now lookAt and update are in sync. Only using euler angles for update deltas now. Using proto cam in glfw_optix (uses pos & orientation frame only) & hybrid render (uses view matrix only)

--- src/Scene/proto_camera.cpp
+++ src/Scene/proto_camera.cpp
@@ -1,6 +1,6 @@
 
 #include "proto_camera.h"
-#include <glm/ext.hpp>
+
 
 using namespace Scene;
 
@@ -35,10 +35,7 @@ FirstPersonCamera::FirstPersonCamera()
 	,far_dist (1000.f)
 	,movementUnitsPerSecond (5.f)
 	,mouseDegreesPerSecond(10.f)
-	,hang (0.f)
-	,vang (0.f)
-	,oldmousx (0.f)
-	,oldmousy (0.f)
+	,old_mouse ( glm::vec2(0.f) )
 {
 }
 
@@ -52,58 +49,32 @@ void FirstPersonCamera::lookAt( const glm::vec3& pos, const glm::vec3& target, c
 {
 	setPos(pos);
 	world_to_view = glm::lookAt( pos, target, up );
+	orientation = glm::quat_cast(world_to_view); // keep Quat & Matrix in sync
 }
 
 void FirstPersonCamera::update(bool left_key, bool right_key, bool back_key, bool forwards_key,
-	float mouse_x, float mouse_y, bool mouse_is_down, float delta )
+	glm::vec2 mouse_coords, bool mouse_is_down, float delta )
 {
-	float mouse_speed_x = delta * mouseDegreesPerSecond * (mouse_x - oldmousx);
-	float mouse_speed_y = delta * mouseDegreesPerSecond * (mouse_y - oldmousy);
-	oldmousx = mouse_x;
-	oldmousy = mouse_y;
 	if ( mouse_is_down )
 	{	
-        hang -= glm::radians(mouse_speed_x);
-		vang -= glm::radians(mouse_speed_y);
+		// Rotation deltas
+		glm::vec2 mouse_speed = delta * mouseDegreesPerSecond * (mouse_coords - old_mouse);
+
+		glm::quat rx = glm::angleAxis(mouse_speed.y, glm::vec3(1.f, 0.f, 0.f));
+		glm::quat ry = glm::angleAxis(mouse_speed.x, glm::vec3(0.f, 1.f, 0.f));
+		orientation = glm::normalize( rx*orientation*ry );
+		world_to_view = glm::mat4_cast(orientation); // keep Quat & Matrix in sync
 	}
-	float speed = delta * movementUnitsPerSecond; // meters per second
-
-    // Direction : Spherical coordinates to Cartesian coordinates conversion
-    glm::vec3 lookDir(
-        cos(vang) * sin(hang),
-        sin(vang),
-        cos(vang) * cos(hang)
-    );
-
-    // Right vector
-    glm::vec3 right = glm::vec3(
-        sin(hang - 3.14f/2.0f),
-        0,
-        cos(hang - 3.14f/2.0f)
-    );
-
-    glm::vec3 up = glm::cross( right, lookDir );
-
+	
     float xMove = float(right_key-left_key);
     float zMove = float(forwards_key-back_key);
-    glm::vec3 moveDir = right*xMove + lookDir*zMove;
-    if ( glm::length2(moveDir) ) 
+	// Use view matrix direction vectors for FPS-like control
+    glm::vec3 moveDir = getStrafeDirection()*xMove + getLookDirection()*zMove;
+    if ( glm::length2(moveDir) ) {
+		float speed = delta * movementUnitsPerSecond; // meters per second
         pos += speed * glm::normalize(moveDir);
+		world_to_view = glm::translate( glm::mat4_cast(orientation) , -pos);
+	}
 
-    cameraStrafe = right;
-    cameraUp = up;
-    cameraForward = lookDir;
-
-    world_to_view = glm::lookAt( pos, pos+lookDir, up );
-
-	//glm::mat4 rmx = glm::rotate( glm::mat4(1.0f), vang, cameraStrafe );
-	//glm::mat4 rmy = glm::rotate( glm::mat4(1.0f), hang, cameraUp );
-	//glm::mat4 rotMat = rmx * rmy;
-
-	//glm::vec4 moveDir( speed * (left_key-right_key), 0.0f, speed * (forwards_key-back_key), 0.0f );
-	//glm::vec4 lookDir = moveDir * rotMat;
-	//glm::vec3 newPos = getPos() + glm::vec3( lookDir );
-
-	//setPos(newPos);
-	//view = glm::translate(rotMat, newPos);
+	old_mouse = mouse_coords;
 }				  
